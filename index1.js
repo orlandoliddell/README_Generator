2e12d9193677df6de38a57680d07d2ad07378364
filthyningen <malikliddell@gmail.com>
2020-08-25 16:38:05 -0400

    Finishing up questions and tablee of contents.

--- index1.js
+++ index1.js
@@ -1,13 +1,54 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
+var path = require("path");
+var generateMarkDown = require("./utils/generateMarkdown")
 
 inquirer.prompt([
-    {},
-    {}
+    {type:"input" ,
+    message:"What is your title?",
+    name:"title"},
+    
+    {type:"input" ,
+    message:"What is your description?" ,
+    name:"description"},
+    
+    {type:"input" ,
+    message:"How do you install this?" ,
+    name:"installation",
+    default: "npm i"},
+    
+    {type:"input" ,
+    message:"What is your usage?" ,
+    name:"usage"},
+   
+    {type:"input" ,
+    message:"What are you contributing?" ,
+    name:"contribution"},
+    
+    {type:"input" ,
+    message:"What are your instructions?" ,
+    name:"instructions"},
+    
+    {type:"list" ,
+    message:"What is your license?" ,
+    name:"license",
+    choices:["MIT", "APACHE", "GPL", "None"]},
+    
+    {type:"input" ,
+    message:"What is your username?" ,
+    name:"username"},
+    
+    {type:"input" ,
+    message:"What is your email?" ,
+    name:"email"},
+    
+    {type:"input" ,
+    message:"How would a user test?" ,
+    name:"test",
+    default:"npm test"}
+
 ]).then(answers => {
     console.log(answers);
-
-    let readme = "";
 
     fs.writeFile("README.md", readme, err => {
         if (err) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+![badge](https://img.shields.io/badge/LICENSE-${data.license}-TEAL)
+
+##Description
+${data.description}
+
 
 `;
 }
